c762bb5732e88fd8ba7540b7d0e0a49916844b34
ashtonguevarra <ashtonguevarra1@gmail.com>
2026-03-15 11:17:23 +0800

    include admin  email

--- config.py
+++ config.py
@@ -11,4 +11,4 @@ MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
 MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
 MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-ADMINS = ['your-email@example.com']
+ADMINS = ['ashtonbreadserk@gmail.com']
