11553ca69f39165967a681f6f7e7c8a5f489335d
ashtonguevarra <ashtonguevarra1@gmail.com>
2026-03-15 11:09:01 +0800

    import error handlers

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
+from app import routes, models, errors
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,4 +12,3 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
