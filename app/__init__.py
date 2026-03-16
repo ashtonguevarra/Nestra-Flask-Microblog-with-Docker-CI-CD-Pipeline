2d8403f25c0e224e8a699eeacbb3f6dd7411341a
ashtonguevarra <ashtonguevarra1@gmail.com>
2026-03-16 22:27:13 +0800

    logging to a file

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask_login import LoginManager
 from config import Config
 import logging
 from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -24,11 +26,21 @@ if app.config['MAIL SERVER']:
     mail_handler = SMTPHandler(
         mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
         fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-        toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+        toaddrs=app.config['ADMINS'], subject='Nestra Failure',
         credentials=auth, secure=secure)
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
 
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/nestra.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Nestra startup')
 
 from app import routes, models, errors
 
