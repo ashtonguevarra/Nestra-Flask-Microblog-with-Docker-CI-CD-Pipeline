9a6042d0719dfd2c110928ce17a761e037165dd8
ashtonguevarra <ashtonguevarra1@gmail.com>
2026-03-16 22:45:04 +0800

    update

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, \
+    TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, \
+    Length
 import sqlalchemy as sa
 from app import db
 from app.models import User
-from wtforms import TextAreaField
-from wtforms.validators import Length
 
 
 class LoginForm(FlaskForm):
@@ -35,6 +35,7 @@ class RegistrationForm(FlaskForm):
         if user is not None:
             raise ValidationError('Please use a different email address.')
 
+
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
@@ -48,5 +49,5 @@ class EditProfileForm(FlaskForm):
         if username.data != self.original_username:
             user = db.session.scalar(sa.select(User).where(
                 User.username == username.data))
-        if user is not None:
-            raise ValidationError('Please use a different username.')
+            if user is not None:
+                raise ValidationError('Please use a different username.')
