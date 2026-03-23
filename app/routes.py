69d4283409f5ee6d9bb4c516bbd6fadb3e3e3a7b
ashtonguevarra <ashtonguevarra1@gmail.com>
2026-03-23 10:41:41 +0800

    post submission form in index view function

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,8 @@ import sqlalchemy as sa
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
 from app.models import User
+from app.forms import PostForm
+from app.models import Post 
 
 
 @app.before_request
@@ -19,6 +21,13 @@ def before_request():
 @app.route('/index')
 @login_required
 def index():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.date, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
     posts = [
         {
             'author': {'username': 'John'},
